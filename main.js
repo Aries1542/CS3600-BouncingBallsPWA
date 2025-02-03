b26866265b9c345d174f70c891f8cdb908d142c3
Nick de Jong <aries1542@Large-Lion>
2025-02-02 19:22:21 -0700

    Made iOS permission button readable

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ main();
 async function main() {
 	console.log('This is working');
 
-
 	
+
 	let gravity = [0, 0]
 	if (DeviceOrientationEvent && 
 		typeof DeviceOrientationEvent.requestPermission === 'function') {
 		// iOS 13+
 
 		const button = document.createElement('button');
-		button.innerText = 'Please grant device orientation permission to use this app!';
+		button.innerText = 'Please grant device orientation permission to enable gravity!';
+		button.style.fontSize = '24px';
 		button.style.borderWidth = '5px';
 		button.style.borderRadius = '10px';
 		button.style.backgroundColor = 'rgba(200, 100, 100, 0.7)';
-		button.style.color = 'white';
 
 		button.style.width = '50%';
 		button.style.height = '20%';
